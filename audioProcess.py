10cd09242216a2562e39aa8ca4397ada82c45617
PeterKStemWorld <peter@stemworldeducationalservices.com>
2023-11-05 01:09:29 -0400

    added python files

---
+++ audioProcess.py
@@ -0,0 +1,59 @@
+
+import openai
+from google.cloud import storage
+from google.cloud import speech
+
+openai.api_key = "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"
+
+def audioprocess(audio_file):
+
+    # setup for cloud storage of the audio recording
+    client = storage.Client(project='testproject-404117')
+
+
+    bucket=client.get_bucket('test-bucket-321332173982');  
+    blob=bucket.blob("audio.flac")
+    blob.upload_from_file(audio_file)
+
+    # setup for processing the speech
+    def speech_to_text(
+        config: speech.RecognitionConfig,
+        audio: speech.RecognitionAudio,
+    ) -> speech.RecognizeResponse:
+        client = speech.SpeechClient()
+
+        # Synchronous speech recognition request
+        response = client.recognize(config=config, audio=audio)
+
+    # return response
+        return response.results[0].alternatives[0].transcript
+
+
+    config = speech.RecognitionConfig(
+        language_code="en",
+        audio_channel_count=2
+    )
+    audio = speech.RecognitionAudio(
+        uri="gs://test-bucket-321332173982/audio.flac",
+    )
+    response = speech_to_text(config, audio)
+    print(response)
+    #print(str(response))
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+            "content": """You are my assistant and I am rambling to you about all the tasks I need to do. 
+            Create a list of bullet points of all the different tasks I told you. If a date has been mentioned include it
+            in parentheses at the end of the line."""},
+            {"role": "user", "content": response}
+        ]
+    )
+    print(completion.choices[0].message.content)
+    #return completion.choices[0].message.content
+    return "baka"
+    
+
+
+#print(audioprocess("audiotest2.wav"))
---
+++ backend.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+from audioProcess import audioprocess
+
+app = Flask(__name__)
+
+@app.route('/upload-audio', methods=['POST'])
+def upload_audio():
+    if 'audio' not in request.files:
+        return jsonify({'message': 'No audio file part'})
+
+    audio_file = request.files['audio']
+    print(audio_file)
+    if audio_file.filename == '':
+        return jsonify({'message': 'No selected file'})
+
+    # Process the audio file, e.g., save it, perform analysis, etc.
+    # Ensure you have a folder for audio storage if needed.
+    print(audio_file.filename)
+    return jsonify({'message': audioprocess(audio_file)})
+
+if __name__ == '__main__':
+    app.run()
